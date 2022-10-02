40176bcfddf4035ec949c0062c15d4c0fba1be04
nexiss <nexiss@users.noreply.github.com>
2022-10-03 01:40:26 +0200

    Generate random attributes

--- myz-character-generator/src/store/store.utils.ts
+++ myz-character-generator/src/store/store.utils.ts
@@ -1,9 +1,6 @@
-import { Role, Mutation } from '../models';
+import { Role, Mutation, Attributes, Attribute } from '../models';
 import { ROLE_OPTION_VALUE, RANDOM } from './data';
 import { Current } from './store';
-
-// TODO: Filter is needed because Object.values is returning also keys
-const filterNumbers = (n: any) => typeof n === 'number';
 
 export const getRandomRole = (): Role => {
   const enumValues = Object.values(Role).filter(filterNumbers) as Role[];
@@ -33,12 +30,42 @@ export const generateRandomCurrent = (
       return {
         ...current,
         role: getRandomRole(),
-        attributes: {
-          strength: 0,
-          agility: 1,
-          wits: 2,
-          empathy: 3,
-        },
+        attributes: generateRandomAttributes(),
       };
   }
+};
+
+const MAX_ATTRIBUTE = 4;
+
+// TODO: Filter is needed because Object.values is returning also keys
+const filterNumbers = (n: any) => typeof n === 'number';
+
+const generateRandomAttributes = (): Attributes => {
+  return Object.values(Attribute).reduce((prev, attribute: Attribute) => {
+    const isAnyAttributeMax = Object.values(prev).some(
+      (at) => at === MAX_ATTRIBUTE
+    );
+
+    return {
+      ...prev,
+      [attribute]: isAnyAttributeMax
+        ? getRandomInt(1, MAX_ATTRIBUTE - 1)
+        : getBestRandomAttributeOf(3),
+    } as Attributes;
+  }, {} as Attributes);
+};
+
+const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max + 1); // Adding 1 because maximum is exclusive
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+};
+
+const getBestRandomAttributeOf = (bestOf: number): number => {
+  let best = 0;
+  for (let i = 0; i < bestOf; i++) {
+    const current = getRandomInt(1, MAX_ATTRIBUTE);
+    best = current > best ? current : best;
+  }
+  return best;
 };
